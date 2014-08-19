efce28cc522b1ad4bb8919a1279fcf5b69eceb80
Dokespike <dokespike@gmail.com>
2014-08-19 19:29:30 -0400

    Update PyPlayer.py

--- PyPlayer.py
+++ PyPlayer.py
@@ -9,13 +9,86 @@ from Tkinter import *
 
 
 
-#Creates TKinter window and starts its loop
+#This is what makes PyPlayer work, playing the files, organizing it in the program window, and saving it in a dirrectorys file. It does EVERYTHING KNOWN TO MAN. (kind of)
+
+class pyplayer:
+
+	def initplay(self,song):
+		print(song)
+		pygame.mixer.music.load(song)
+		pygame.mixer.music.play()
+		curts.set('Now Playing: ' + song[len(songfolder):])
+		print('Now Playing: ' + song[len(songfolder):])
+		print curts.get
+		return
+	
+	
+	    
+	def Open(self):
+		myopen = tkFileDialog.askopenfilename()
+		if len(myopen) < 4:
+			noneadded = tkMessageBox.showwarning( message = 'No song 	added')
+			curts.set('              No song added')
+		if CheckForSameSong == '1':
+			numberofsongs = len(musiclist)
+			samesong = False
+			x = 0
+			for song in musiclist:
+				x += 1
+				if song == myopen:
+					samesong = True
+				if x == numberofsongs:
+					if samesong == True:
+						alreadyhere = 	tkMessageBox.showwarning( message = 'Song already added')
+					else:
+						Pp.addsong(myopen)
+						listbox.insert(END, myopen[len	(songfolder):])
+						Pp.initplay(myopen)
+		
+		else:
+			Pp.addsong(myopen)
+			listbox.insert(END, myopen[len(songfolder):])
+			Pp.initplay(myopen)
+
+	def delete(self):
+		print get(ACTIVE)
+		listbox.delete(ANCHOR)
+		CSI = open('MusicDir.txt', 'w')
+		for song in listbox:
+			CSI.write(song+'\n')
+			CSI.close()
+
+
+	def addsong(self,x):
+		CSI = open('MusicDir.txt', 'a')
+		CSI.write(x+'\n')
+		CSI.close()
+	
+	def SetVolume(self):
+		volumeSlider = volume.get() + .0
+		newVolume = volumeSlider/100
+		pygame.mixer.music.set_volume(newVolume)
+
+	
+	def quit():
+		exit = tkMessageBox.askyesno(title="Quit", message = 'Are you sure')
+		if exit == True:
+			app.destroy()
+			sys.exit()
+			return
+
+
+
+
+
+
+
+#Creates TKinter and starts its loop, makes Pp to use the above class (pyplayer), starts pygames mixer (used to play sounds), and makes a list used to store info about the songs in the program.
 musiclist = []
+Pp = pyplayer()
 app = Tk()
 app.title('PyPlayer')
 app.geometry('750x500')
-
-#Starts pygame mixer
 pygame.mixer.init()
 
 
@@ -26,7 +99,7 @@ listbox.grid(row=1, column=0 )
 
 
 
-#Opens the config file and puts each line into list 'confg'
+#Opens the config file and puts each line into the list 'confg'
 #The [:-1] removes the break symbol from the end of each line (/n)
 confg = []
 conf = open('Config.txt')
@@ -37,12 +110,17 @@ conf.close()
 
 
 #Grabbing relevent info from the confg list to use in program
-#The chopping of the strings removes non-needed text
+#The chopping in these strings removes non-needed text
 songfolder = confg[0][13:]
 CheckForSameSong = confg[1][25:]
 VolumeControl = confg[2][21:]
 Logo = confg[3][21:]
 ArrowBrowser = confg[4][35:]
+
+
+
+#This prints out info about the config to console.
+print('\nPyPlayer is Licensed under the MIT license (Do what ever you want nerd!)\n\nLoaded config is as follows:\n===============================\nsongfolder: ' + songfolder +'\nCheckForSameSong: ' +  CheckForSameSong + '\nVolumeControl: ' + VolumeControl + '\nLogo: ' + Logo + '\nArrowBrowser: ' + ArrowBrowser + '\n===============================\n')
 
 
 
@@ -57,75 +135,10 @@ songdir.close()
 
 
 #On select in listbox, take songfolderimp plus song name(value) and play it
-def CurSelet(evt):
+def CurSelect(evt):
     value=(listbox.get(listbox.curselection()))
-    initplay(songfolder + value)
-listbox.bind('<<ListboxSelect>>',CurSelet)
-
-
-
-def initplay(song):
-	pygame.mixer.music.load(song)
-	pygame.mixer.music.play()
-	curts.set('Now Playing: ' + song[len(songfolder):])
-	print curts.get
-	return
-
-
-    
-def Open():
-	myopen = tkFileDialog.askopenfilename()
-	if len(myopen) < 4:
-		noneadded = tkMessageBox.showwarning( message = 'No song added')
-		curts.set('              No song added')
-	if CheckForSameSong == '1':
-		numberofsongs = len(musiclist)
-		print numberofsongs
-		samesong = False
-		x = 0
-		for song in musiclist:
-			x += 1
-			if song == myopen:
-				samesong = True
-			if x == numberofsongs:
-				if samesong == True:
-					alreadyhere = tkMessageBox.showwarning( message = 'Song already added')
-				else:
-					addsong(myopen)
-					listbox.insert(END, myopen[len(songfolder):])
-					initplay(myopen[len(songfolder):])
-	
-	else:
-		addsong(myopen)
-		listbox.insert(END, myopen[len(songfolder):])
-		initplay(myopen[len(songfolder):])
-
-def delete():
-	print get(ACTIVE)
-	listbox.delete(ANCHOR)
-	CSI = open('MusicDir.txt', 'w')
-	for song in listbox:
-		CSI.write(song+'\n')
-		CSI.close()
-
-
-def addsong(x):
-	CSI = open('MusicDir.txt', 'a')
-	CSI.write(x+'\n')
-	CSI.close()
-	
-def SetVolume():
-	volumeSlider = volume.get() + .0
-	newVolume = volumeSlider/100
-	pygame.mixer.music.set_volume(newVolume)
-
-	
-def quit():
-	exit = tkMessageBox.askyesno(title="Quit", message = 'Are you sure')
-	if exit == True:
-		app.destroy()
-		sys.exit()
-		return
+    Pp.initplay(songfolder + value)
+listbox.bind('<<ListboxSelect>>',CurSelect)
 		
 
 
@@ -151,7 +164,7 @@ if ArrowBrowser == '1':
 if VolumeControl == '1':
 	volume = Scale(app, from_=0, to=100, length=200, orient=HORIZONTAL)
 	volume.place(relx=.62, rely=.01)
-	VolumeButton = Button(app, text= 'Volume', command = SetVolume).place(relx=.9, rely=.03)
+	VolumeButton = Button(app, text= 'Volume', command = Pp.SetVolume).place(relx=.9, rely=.03)
 
 
 
@@ -174,9 +187,9 @@ else:
 #Jazz hands
 menubar = Menu(app)
 filemenu = Menu(menubar, tearoff = 0)
-filemenu.add_command(label = 'Import', command = Open)
-filemenu.add_command(label = 'Delete', command = delete)
-filemenu.add_command(label = 'Close',command = quit)
+filemenu.add_command(label = 'Import', command = Pp.Open)
+filemenu.add_command(label = 'Delete', command = Pp.delete)
+filemenu.add_command(label = 'Close',command = Pp.quit)
 menubar.add_cascade(label = 'File', menu = filemenu)
 app.config(menu = menubar)
 
